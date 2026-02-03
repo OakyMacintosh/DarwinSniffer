fea487490129590d2dcdf17af8d884808cdb44f0
OakyMacintosh <migsufrem@gmail.com>
2026-02-03 19:00:23 -0300

    Initial Commit

---
+++ DarwinSniffer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DarwinSniffer
+//
+//  Created by Oaky on 03/02/26.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
